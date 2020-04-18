891570d3508762e9481dace130d6be4b2dc75b18
Kion F <fallah.kion@gmail.com>
2020-04-18 17:09:53 -0400

    Update Speed Test for Sparse Multiplication

--- speed_test.py
+++ speed_test.py
@@ -2,45 +2,32 @@ import argparse
 import torch
 import torch.optim as optim
 
+import time
+
 import datasets
 from network import Network, Baseline
 
 
 def test_speed(net:Network, data:datasets.Dataset):
-	for (x, y) in data.train:
-		x, y = x.to(net.device), y.to(net.device)
-		theta = net(x)
+    pre_time = time.time()
+	for x, _ in data.train:
+		x = x.to(net.device)
+        y_hat = net.sparse_forward(x)
 
-		optimizer.zero_grad()
-		loss = net.loss(theta, y)
-
-		loss.backward()
-		optimizer.step()
-
-		if iteration % 100 == 0:
-			print(f'[{epoch:3}] {iteration:6} || Loss: {loss.item():.3f}')
-
-		iteration += 1
-
-	print()
-	acc = test(net, data.test)
-	print(f'Test  Accuracy: {acc:.2f}%')
-	print()
-
-	net.train()
-
-	return acc
+	return (time.time()) - pre_time) / data.train.__length__()
 
 
 if __name__ == '__main__':
-	dataset = datasets.MNIST()
+    batch_size  = 64
+	dataset = datasets.MNIST(batch_size=batch_size)
 
 	net = Baseline(dataset.shape, dataset.num_classes)
 	net.to('cuda')
 
 	try:
-		train(net, dataset)
+		run_time = test_speed(net, dataset)
+        print("Average runtime over {} samples with batch size {}: {:.3f} sec".format(
+        dataset.train.__length__(), batch_size, run_time))
+
 	except KeyboardInterrupt:
 		print('Stopping...')
-
-	net.save_weights('weights/final_weights.pth')
